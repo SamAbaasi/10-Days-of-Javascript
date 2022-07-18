b6c9e36ce60d97701030234b9b8340ee562e56c3
Saman Abbasi <samyar1992@gmail.com>
2022-07-18 07:58:00 +0430

    Day 2

---
+++ Day 2/answers.js
@@ -0,0 +1,54 @@
+// # Conditional Statements: If-Else
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    if(score >= 0 && score <= 5) {
+        grade = 'F'
+    } else if(score > 5 && score <= 10) {
+        grade = 'E'
+    } else if(score > 10 && score <= 15) {
+        grade = 'D'
+    } else if(score > 15 && score <= 20) {
+        grade = 'C'
+    } else if(score > 20 && score <= 25) {
+        grade = 'B'
+    } else if(score > 25 && score <= 30) {
+        grade = 'A'
+    }
+    return grade;
+}
+// # Conditional Statements: Switch
+function getLetter(s) {
+    let letter;
+    switch (true) {
+        case 'aeiou'.includes(s[0]):
+            letter = 'A';
+            break;
+        case 'bcdfg'.includes(s[0]):
+            letter = 'B';
+            break;
+        case 'hjklm'.includes(s[0]):
+            letter = 'C';
+            break;
+        case 'npqrstvwxyz'.includes(s[0]):
+            letter = 'D';
+            break;
+    }
+    return letter;
+}
+// # Loops
+function vowelsAndConsonants(s) {
+    let vowels = ["a", "e", "i", "o", "u"];
+
+    for(let v of s) {
+        if(vowels.includes(v)) {
+            console.log(v);
+        }
+    }
+    
+    for(let v of s) {
+        if(!vowels.includes(v)) {
+            console.log(v);
+        }
+    }
+}
